97e99e98fdcc4b257a775d723bb95c40a0b68091
Mary West <westmarye1219@gmail.com>
2019-04-08 21:26:13 -0500

    store component prints to page

---
+++ javascripts/components/store.js
@@ -0,0 +1,9 @@
+import util from '../helpers/util.js';
+
+const makeStore = () => {
+  let domString = '<h3>Help</h3>';
+  util.printToDom('store-container', domString);
+
+};
+
+export default { makeStore }
